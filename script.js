bd747c5a8f6aaf1299013bf05feccc23ef7f13a0
wallacevieira <wallaceofc@hotmail.com>
2024-08-15 14:13:04 -0300

    feat: exercicio proposto

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const numsSorteados = [];
-const btnReiniciar = buscarPeloId("btn-reiniciar");
+// Buscar botão reiniciar pelo id
 
 function manuseiaEstilosDoBotao() {
   if (numsSorteados.length > 0) {
@@ -9,40 +8,4 @@ function manuseiaEstilosDoBotao() {
     btnReiniciar.classList.remove("container__botao");
     btnReiniciar.classList.add("container__botao-desabilitado");
   }
-}
-
-function buscarPeloId(nomeDoId) {
-  return document.getElementById(nomeDoId);
-}
-
-function gerarNumeroAleatorio(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min) + 1;
-}
-
-function sortear() {
-  const qtsNumSortear = buscarPeloId("quantidade").value;
-  const de = buscarPeloId("de").value;
-  const ate = buscarPeloId("ate").value;
-
-  for (let i = 0; i < qtsNumSortear; i++) {
-    const numAleatorio = gerarNumeroAleatorio(de, ate);
-
-    numsSorteados.push(numAleatorio);
-  }
-
-  mostrarResultado();
-  manuseiaEstilosDoBotao();
-}
-
-function mostrarResultado() {
-  const resultado = buscarPeloId("resultado");
-  resultado.innerHTML = `<p class="texto__paragrafo">
-    ${numsSorteados.join(", ")}
-  </p>`;
-}
-
-function reiniciar() {
-  numsSorteados.length = 0;
-  mostrarResultado();
-  manuseiaEstilosDoBotao();
 }
